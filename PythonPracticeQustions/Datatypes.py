bb7f344f0460bcdf7dbf6d648b8f3b63061cf3fa
root <ShreyasAmbre>
2020-06-12 17:29:22 +0530

    Datatype program

---
+++ PythonPracticeQustions/Datatypes.py
@@ -0,0 +1,25 @@
+print("We will learn about python datatypes")
+
+a = "Shreyas"
+b = 12
+c = 12.3
+d = True
+# List :- smilar as array
+e = [12, 14, "Ambre", True]
+
+# tuple :- are unchangeable can access tuple using index value or range
+f = (12, 99, 81, 14, 3)
+print(f)
+print(type(f))
+
+#  set:- doesnot have index value so it access only using for loop
+g = {"apple", "Orange", "Jackfruit"}
+
+# Dict :- Changeable and has index
+
+car = {
+    "model": 1001,
+    "name": "BMW",
+    "price": 3100000
+}
+print(car["model"])
